f64c2a10c59b0e65f339ab2571210682c4ad2e41
tmbern <tmbern42@gmail.com>
2020-06-17 13:40:21 -0600

    completed stocks.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # given an array of stock prices, we want to find the max profit if we bought
+  # on one day and sold at another day. 
+  # could itterate thru the list and subtract the first element from each of the following elements.
+  # All of those would be added to an list (possible profits). 
+  # Take the maximum of that list.
+  possible_profit = []
 
+  for i in range(len(prices[:-1]) - 1):
+      for j in range(i, len(prices[:-1])):
+          possible_profit.append(prices[j+1] - prices[i])
+
+  return max(possible_profit)
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
