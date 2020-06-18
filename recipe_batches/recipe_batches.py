4c23213b94b596b7308545226076fa5843c57bc2
tmbern <tmbern42@gmail.com>
2020-06-18 13:34:19 -0600

    completed recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,9 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+   # Divide the ingredient amounts by the recipe amount
+   # if the amount is greater than or equal to 1 for all values, then you have enough.
+   # if we round down to the nearest integer, then the total batches that we can make
+   # will be the smallest integer in that array. 
 
+    batch_arr = []
+    for key, value in recipe.items():
+        if key in ingredients:
+            batch_arr.append(ingredients[key] // recipe[key])
+        else:
+            batch_arr.append(0)
 
+    return min(batch_arr)
+  
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
